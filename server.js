3d638f6a086c36c1bdc7982fb9839c68f8eba910
krishna128kk <krishnakk123004@gmail.com>
2025-05-30 11:25:01 +0530

    Update server.js with improvements

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ app.post('/prediction', [
     }),
 ], async (req, res) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
